aeacf865af91fbfdb39465f645c215ecc1d732fc
Meet Godhani <meetgodhani23@gmail.com>
2020-02-28 20:47:59 -0500

    Fixes

--- src/components/Peep.vue
+++ src/components/Peep.vue
@@ -1,5 +1,6 @@
 <template>
     <svg width="100%" height="100%" viewBox="0 0 1136 1533" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet">
+      <path v-if="transparent" d="M0 0H1136V1533H0V0Z" fill="white"/>
       <component :is="body"/>
       <component :is="head"/>
       <component :is="face"/>
@@ -22,6 +23,10 @@ export default {
       default () {
         return 'BodyWhatever'
       }
+    },
+    transparent: {
+      type: Boolean,
+      default: false,
     },
     face: {
       type: String,
