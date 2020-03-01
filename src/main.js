181ac95e68c841566d1a2c4582f1c6c5e622ac01
Meet Godhani <meetgodhani23@gmail.com>
2020-02-29 23:59:17 -0500

    Fixes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { BootstrapVue } from 'bootstrap-vue'
+import VueAnalytics from 'vue-analytics'
+import { ANALYTICS } from './config'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
@@ -10,6 +12,9 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
+Vue.use(VueAnalytics, {
+  id: ANALYTICS
+})
 
 const files = require.context('./', true, /\.vue$/i)
 files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
