474b2b63ee418ec721b9d400e5f13b31c121932a
Meet Godhani <meetgodhani23@gmail.com>
2020-03-01 11:52:36 -0500

    fix

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // ...other vue-cli plugin options...
+  pwa: {
+    name: 'Get Peeps',
+    themeColor: '#fff',
+    msTileColor: '#fff',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black'
+  }
+}
